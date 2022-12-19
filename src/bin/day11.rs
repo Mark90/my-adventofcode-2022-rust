61c02bf0df528e8f36f760505a148cbb6b23fb0d
Mark90 <mark_moes@live.nl>
2022-12-19 22:16:32 +0100

    Day 11 part 2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -13,17 +13,17 @@ enum Operator {
 #[derive(Debug)]
 enum Target {
     SELF,
-    STATIC(i32),
+    STATIC(u64),
 }
 
 struct Monkey {
-    inventory: Vec<i32>,
+    inventory: Vec<u64>,
     operation_target: Target,
     operation_operator: Operator,
-    test_value: i32,
-    test_true: i32,
-    test_false: i32,
-    items_inspected: i32,
+    test_value: u64,
+    test_true: u64,
+    test_false: u64,
+    items_inspected: u64,
 }
 
 impl fmt::Display for Monkey {
@@ -37,7 +37,7 @@ impl fmt::Display for Monkey {
 }
 
 impl Monkey {
-    fn inspect_item(&self, old: i32) -> i32 {
+    fn inspect_item(&self, old: u64) -> u64 {
         match (&self.operation_target, &self.operation_operator) {
             (Target::SELF, Operator::ADD) => old + old,
             (Target::SELF, Operator::MULTIPLY) => old * old,
@@ -45,7 +45,7 @@ impl Monkey {
             (Target::STATIC(value), Operator::MULTIPLY) => old * value,
         }
     }
-    fn throw_item_to(&self, item_worry_level: i32) -> i32 {
+    fn throw_item_to(&self, item_worry_level: u64) -> u64 {
         if item_worry_level % self.test_value == 0 {
             return self.test_true;
         }
@@ -53,17 +53,16 @@ impl Monkey {
     }
 }
 
-fn get_last_int(line: &str) -> i32 {
+fn get_last_int(line: &str) -> u64 {
     line.split_ascii_whitespace()
         .last()
         .unwrap()
-        .parse::<i32>()
+        .parse::<u64>()
         .unwrap()
 }
 
-fn part1(content: &str) -> i32 {
+fn get_monkeys_from_input(content: &str) -> Vec<Monkey> {
     let mut monkeys = Vec::new();
-
     for lines in content.split("\n\n") {
         let mut split_lines = lines.lines().skip(1);
         let starting = split_lines
@@ -73,9 +72,9 @@ fn part1(content: &str) -> i32 {
             .skip(1)
             .next()
             .unwrap();
-        let items: Vec<i32> = starting
+        let items: Vec<u64> = starting
             .split(", ")
-            .map(|c| c.parse::<i32>().unwrap())
+            .map(|c| c.parse::<u64>().unwrap())
             .collect();
         let line_operation: Vec<&str> = split_lines
             .next()
@@ -96,7 +95,7 @@ fn part1(content: &str) -> i32 {
         };
         let operation_target = match target {
             "old" => Target::SELF,
-            value => Target::STATIC(value.parse::<i32>().unwrap()),
+            value => Target::STATIC(value.parse::<u64>().unwrap()),
         };
         monkeys.push(Monkey {
             inventory: items,
@@ -108,70 +107,80 @@ fn part1(content: &str) -> i32 {
             items_inspected: 0,
         })
     }
-
-    println!("Before start");
-    log_inventories(&monkeys);
-
-    let mut round = 0;
-    loop {
-        round += 1;
-        monkey_business(&mut monkeys);
-        println!("After round {}", round);
-        log_inventories(&monkeys);
-
-        if round == 20 {
-            break;
-        }
-    }
-
-    let mut item_inspections: Vec<i32> = monkeys.iter().map(|m| m.items_inspected).collect();
-    item_inspections.sort();
-    let highest = &item_inspections[item_inspections.len() - 2..];
-    highest[0] * highest[1]
+    monkeys
 }
 
-fn log_inventories(monkeys: &Vec<Monkey>) {
-    for (i, m) in monkeys.iter().enumerate() {
-        println!("monkey {}: {}", i, m);
-    }
-}
-
-fn monkey_business(monkeys: &mut [Monkey]) {
-    for i in 0..monkeys.len() {
-        // println!("Monkey {}'s turn", i);
-        let mut monkey = &mut monkeys[i];
-        let items_thrown = monkey_turn(&mut monkey);
-        for (monkey_id, item_wl) in items_thrown {
-            // Append thrown items to the end of the recipient monkey's inventory
-            monkeys[monkey_id as usize].inventory.push(item_wl);
-        }
-    }
-}
-
-fn monkey_turn(monkey: &mut Monkey) -> Vec<(i32, i32)> {
+fn monkey_turn(monkey: &mut Monkey, combined_modulo: u64) -> Vec<(u64, u64)> {
     // Keep track of monkeyids/items thrown by this monkey
     // (cannot modify `monkeys` here due to borrowing restrictions)
-    let mut items_thrown: Vec<(i32, i32)> = Vec::new();
+    let mut items_thrown: Vec<(u64, u64)> = Vec::new();
     for &item_wl_old in monkey.inventory.iter() {
-        let item_wl_new = ((monkey.inspect_item(item_wl_old) as f32) / 3_f32).floor() as i32;
-        // println!("{} worry level is now {}", item_wl_old, item_wl_new);
+        let mut item_wl_new = monkey.inspect_item(item_wl_old);
+        if combined_modulo == 0 {
+            // part 1
+            item_wl_new = (item_wl_new as f32 / 3_f32).floor() as u64;
+        } else {
+            // part 2: apply product of all test values as modulo, because that won't change the 'throw item to' division
+            item_wl_new = item_wl_new % combined_modulo;
+        }
         let monkey_id = monkey.throw_item_to(item_wl_new);
         items_thrown.push((monkey_id, item_wl_new));
     }
-    monkey.items_inspected += items_thrown.len() as i32;
+    monkey.items_inspected += items_thrown.len() as u64;
     // Clear this monkey's inventory
     monkey.inventory = Vec::new();
     items_thrown
 }
 
-fn part2(content: &str) -> i32 {
-    0
+fn monkey_business(monkeys: &mut [Monkey], combined_modulo: u64) {
+    for this_monkey in 0..monkeys.len() {
+        let mut monkey = &mut monkeys[this_monkey];
+        let items_thrown = monkey_turn(&mut monkey, combined_modulo);
+        for (other_monkey, item_wl) in items_thrown {
+            monkeys[other_monkey as usize].inventory.push(item_wl);
+        }
+    }
+}
+
+fn part1(content: &str) -> u64 {
+    let mut monkeys = get_monkeys_from_input(content);
+    let mut round = 0;
+    loop {
+        round += 1;
+        monkey_business(&mut monkeys, 0);
+        if round == 20 {
+            break;
+        }
+    }
+
+    let mut item_inspections: Vec<u64> = monkeys.iter().map(|m| m.items_inspected).collect();
+    item_inspections.sort();
+    let highest = &item_inspections[item_inspections.len() - 2..];
+    highest[0] as u64 * highest[1] as u64
+}
+
+fn part2(content: &str) -> u64 {
+    let mut monkeys = get_monkeys_from_input(content);
+    let combined_modulo: u64 = monkeys.iter().map(|m| m.test_value).product();
+    let mut round = 0;
+    loop {
+        round += 1;
+        monkey_business(&mut monkeys, combined_modulo);
+        if round == 10000 {
+            break;
+        }
+    }
+
+    let mut item_inspections: Vec<u64> = monkeys.iter().map(|m| m.items_inspected).collect();
+    item_inspections.sort();
+    let highest = &item_inspections[item_inspections.len() - 2..];
+    highest[0] as u64 * highest[1] as u64
 }
 
 fn main() {
     let content = read_to_string(utils::get_path(DAY, false)).expect("File not found");
     println!("part1 {}", part1(&content)); // 58322
-    println!("part2 {}", part2(&content));
+    println!("part2 {}", part2(&content)); // 13937702909
 }
 
 #[cfg(test)]
@@ -182,12 +191,12 @@ mod tests {
     #[test]
     fn test_part_1() {
         let content = read_to_string(utils::get_path(DAY, true)).expect("File not found");
-        assert_eq!(part1(&content), 10605);
+        assert_eq!(part1(&content), 10605u64);
     }
 
-    // #[test]
-    // fn test_part_2() {
-    //     let content = read_to_string(utils::get_path(DAY, true)).expect("File not found");
-    //     assert_eq!(part2(&content), -1);
-    // }
+    #[test]
+    fn test_part_2() {
+        let content = read_to_string(utils::get_path(DAY, true)).expect("File not found");
+        assert_eq!(part2(&content), 2713310158u64);
+    }
 }
