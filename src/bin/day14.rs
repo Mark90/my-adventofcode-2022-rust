95bb75727df43b969a1a946281e97b82b2a0b349
Mark90 <mark_moes@live.nl>
2022-12-28 15:40:07 +0100

    Day 14 part 2

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -3,7 +3,7 @@ use std::{collections::HashSet, fs::read_to_string};
 
 const DAY: &str = "day14";
 
-fn part1(content: &str) -> i32 {
+fn parse_grid(content: &str) -> HashSet<(i32, i32)> {
     let mut grid: HashSet<(i32, i32)> = HashSet::new();
     for blockline in content.lines() {
         let coords: Vec<(i32, i32)> = blockline
@@ -13,42 +13,32 @@ fn part1(content: &str) -> i32 {
             .collect();
         let mut prev = coords.first().unwrap();
         for coord in coords.iter().skip(1) {
-            // println!("Draw from {:?} to {:?}", prev, coord);
             let xmin = coord.0.min(prev.0);
             let xmax = coord.0.max(prev.0);
             let ymin = coord.1.min(prev.1);
             let ymax = coord.1.max(prev.1);
             for x in xmin..=xmax {
                 for y in ymin..=ymax {
-                    let boo = (x, y);
-                    // println!("Inserting {boo:?}");
-                    grid.insert(boo);
+                    grid.insert((x, y));
                 }
             }
             prev = coord;
         }
     }
+    grid
+}
 
-    let grid_xmin = grid.iter().map(|(x, _)| *x).min().unwrap();
-    let grid_xmax = grid.iter().map(|(x, _)| *x).max().unwrap();
-    let grid_ymin = 0;
+fn part1(content: &str) -> i32 {
+    let mut grid = parse_grid(content);
+
     let grid_ymax = grid.iter().map(|(_, y)| *y).max().unwrap();
 
     let sand_origin = (500, 0);
 
-    print_grid(
-        grid_ymin,
-        grid_ymax,
-        grid_xmin,
-        grid_xmax,
-        sand_origin,
-        &grid,
-    );
-
     let transformations = [(0, 1), (-1, 1), (1, 1)]; // down, down+left, down+right
 
-    let mut settled_sand: HashSet<(i32, i32)> = HashSet::new();
     let mut settled = true;
+    let mut blocks_settled = 0;
     // Spawn sand blocks until they're not settling anymore (i.e. overflowing)
     while settled {
         let mut sand = sand_origin.clone();
@@ -75,51 +65,66 @@ fn part1(content: &str) -> i32 {
                 // Didn't move -> settled
                 settled = true;
                 grid.insert(sand);
-                settled_sand.insert(sand);
+                blocks_settled += 1;
                 break;
             }
 
             // Successfully moved
             if sand.1 >= grid_ymax {
-                // Overlowing
+                // Overflowing
                 break;
             }
         }
     }
 
-    settled_sand.iter().len() as i32
-}
-
-fn print_grid(
-    grid_ymin: i32,
-    grid_ymax: i32,
-    grid_xmin: i32,
-    grid_xmax: i32,
-    sand_origin: (i32, i32),
-    grid: &HashSet<(i32, i32)>,
-) {
-    for y in grid_ymin..=grid_ymax {
-        for x in grid_xmin..=grid_xmax {
-            if (x, y) == sand_origin {
-                print!("+");
-            } else if grid.contains(&(x, y)) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        print!("\n");
-    }
+    blocks_settled
 }
 
 fn part2(content: &str) -> i32 {
-    0
+    let mut grid = parse_grid(content);
+
+    let grid_ymax = grid.iter().map(|(_, y)| *y).max().unwrap();
+
+    let sand_origin = (500, 0);
+
+    let transformations = [(0, 1), (-1, 1), (1, 1)]; // down, down+left, down+right
+
+    // Spawn sand blocks until the spawn contains settled sand
+    let mut blocks_settled = 0;
+    while !grid.contains(&sand_origin) {
+        let mut sand = (500, 0);
+
+        // Move block until it settles; either on a block, sand, or the floor
+        loop {
+            let mut moved = false;
+
+            // let mut new_position = sand;
+            for transformation in transformations.iter() {
+                let newposition = (sand.0 + transformation.0, sand.1 + transformation.1);
+                if !grid.contains(&newposition) && newposition.1 < (grid_ymax + 2) {
+                    // There is no block on this new position, and we are not at the "floor" yet
+                    sand = newposition;
+                    moved = true;
+                    break;
+                }
+            }
+
+            if !moved {
+                // Didn't move -> settled
+                grid.insert(sand);
+                blocks_settled += 1;
+                break;
+            }
+        }
+    }
+
+    blocks_settled
 }
 
 fn main() {
     let content = read_to_string(utils::get_path(DAY, false)).expect("File not found");
     println!("part1 {}", part1(&content)); // 799
-                                           // println!("part2 {}", part2(&content));
+    println!("part2 {}", part2(&content)); // 29076
 }
 
 #[cfg(test)]
@@ -133,9 +138,9 @@ mod tests {
         assert_eq!(part1(&content), 24);
     }
 
-    // #[test]
-    // fn test_part_2() {
-    //     let content = read_to_string(utils::get_path(DAY, true)).expect("File not found");
-    //     assert_eq!(part2(&content), 140);
-    // }
+    #[test]
+    fn test_part_2() {
+        let content = read_to_string(utils::get_path(DAY, true)).expect("File not found");
+        assert_eq!(part2(&content), 93);
+    }
 }
